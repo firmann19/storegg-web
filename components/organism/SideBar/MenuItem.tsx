4ea5967d794703666afbe5a9cffe0b3013a9c043
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-08-06 14:03:20 +0700

    update siang

---
+++ components/organism/SideBar/MenuItem.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import cx from 'classnames';
+import Image from 'next/image';
+
+interface MenuItemProps {
+    title: string,
+    icon: 'ic-menu-overview' | 'ic-menu-transaction' | 'ic-menu-card' | 'ic-menu-logout' | 'ic-menu-messages' | 'ic-menu-reward' | 'ic-menu-setting'
+    active?: boolean;
+
+}
+
+function MenuItem(props:Partial<MenuItemProps>) {
+  const { title, icon, active } = props;
+  const classItem = cx({
+    item: true,
+    'mb-30': true,
+    active,
+  });
+  return (
+    <div className={classItem}>
+      <div className="me-3">
+        <Image src={`/icon/${icon}.svg`} width={25} height={25} alt="" />
+        <p className="item-title m-0">
+          <a href="" className="text-lg text-decoration-none">{title}</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MenuItem;
